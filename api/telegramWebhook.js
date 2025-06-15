a6e419e0f20d2e684ac1bf0af5176910d0e8575b
MooPanda <moopanda1m@gmail.com>
2025-06-16 02:04:27 +0530

    Added telegramWebhook.js for Telegram bot

--- api/telegramWebhook.js
+++ api/telegramWebhook.js
@@ -2,8 +2,7 @@ export default async function handler(req, res) {
   if (req.method !== 'POST') return res.status(405).send('Only POST allowed');
 
   const body = req.body;
-  console.log("📩 Telegram sent this message:", JSON.stringify(body));
-
+  console.log("📩 Telegram sent:", JSON.stringify(body));
 
   const token = "7942048169:AAEnNrMPJBKWFngn6EQVFQOfk7-bPmm3PfY";
   const telegramApi = `https://api.telegram.org/bot${token}/sendMessage`;
@@ -38,4 +37,3 @@ export default async function handler(req, res) {
 
   res.status(200).send("OK");
 }
-
